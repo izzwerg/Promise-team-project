7750142f86316cc4bb1f356f641bea27a45a269f
Oleksandra K <a.koroliova@gmail.com>
2024-02-09 22:49:30 +0200

    Implement scroll to top

---
+++ src/js/scroll-to-top.js
@@ -0,0 +1,31 @@
+const buttonScrollToTop = document.querySelector('.button-to-top');
+
+function handleScroll() {
+  if (window.scrollY > window.innerHeight / 2) {
+    showButton();
+  } else {
+    hideButton();
+  }
+}
+window.addEventListener('scroll', handleScroll);
+
+function handleClick(event) {
+  event.preventDefault();
+  scrollToTop();
+}
+buttonScrollToTop.addEventListener('click', handleClick);
+
+function scrollToTop() {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth',
+  });
+}
+function showButton() {
+  buttonScrollToTop.style.display = 'block';
+}
+function hideButton() {
+  buttonScrollToTop.style.display = 'none';
+}
+
+// TODO: button is hidden when any pop-up is active (need modal open/close events)
