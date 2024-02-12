5a78229ab39762332ed591000d3bc88b6b1a5ebc
izzwerg <45130552+izzwerg@users.noreply.github.com>
2024-02-12 08:43:18 +0000

    Deploying to gh-pages from @ izzwerg/Promise-team-project@1a1d3f033b7d0898c1b8792996f4b685041d5008 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,4 +1,4 @@
-import{o as h}from"./assets/qoute-9831cd22.js";import"./assets/vendor-8cce9181.js";function u(){let e=JSON.parse(localStorage.getItem("favorites"))||[];const p=e.map(t=>`<li class="fav-ex-item">
+import{o as h}from"./assets/qoute-5539e902.js";import"./assets/vendor-8cce9181.js";function u(){let e=JSON.parse(localStorage.getItem("favorites"))||[];const p=e.map(t=>`<li class="fav-ex-item">
       <div class="fav-ex-item-header">
           <div class="fav-workout-box">
               <p class="fav-workout-text">workout</p>
