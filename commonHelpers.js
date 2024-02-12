1cf0a5546b4485f48d0d6eca7dfa360c6edefa47
izzwerg <45130552+izzwerg@users.noreply.github.com>
2024-02-12 02:17:08 +0000

    Deploying to gh-pages from @ izzwerg/Promise-team-project@5af542b6b96fbdf3820fb13cc47f2373b85c42d3 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,4 +1,4 @@
-import{o as g}from"./assets/qoute-df1053f0.js";import"./assets/vendor-8cce9181.js";function b(){let e=JSON.parse(localStorage.getItem("favorites"))||[];const u=e.map(s=>`<li class="fav-ex-item">
+import{o as g}from"./assets/qoute-df1053f0.js";import"./assets/vendor-8cce9181.js";function x(){let e=JSON.parse(localStorage.getItem("favorites"))||[];const u=e.map(s=>`<li class="fav-ex-item">
       <div class="fav-ex-item-header">
           <div class="fav-workout-box">
               <p class="fav-workout-text">workout</p>
@@ -38,7 +38,7 @@ import{o as g}from"./assets/qoute-df1053f0.js";import"./assets/vendor-8cce9181.j
         <div class="dumbbell-img"></div>
         <p class="no-fav-text">It appears that you haven't added any exercises to your favorites yet. To get started, you can add exercises that you like to your favorites for easier access in the future.</p>
       </div>
-    `,o=window.innerWidth;let a;o<768?a=8:a=e.length;const p=document.querySelector(".no-fav-ex-inner"),d=document.querySelector(".fav-ex-list"),f=document.querySelector(".fav-pag-btn-set");if(e.length===0?p&&(p.innerHTML=v):d&&(d.innerHTML=u,g()),a<e.length&&f){const s=document.createDocumentFragment(),c=Math.ceil(e.length/a);for(let i=1;i<=c;i++){const t=document.createElement("button");t.classList.add("fav-pagination-btn"),t.textContent=i,t.addEventListener("click",()=>{const l=(i-1)*a,h=l+a,m=e.slice(l,h).map(n=>`<li class="fav-ex-item">
+    `,o=window.innerWidth;let a;o<768?a=8:a=e.length;const p=document.querySelector(".no-fav-ex-inner"),d=document.querySelector(".fav-ex-list"),f=document.querySelector(".fav-pag-btn-set");if(e.length===0?p&&(p.innerHTML=v):d&&(d.innerHTML=u,g()),a<e.length&&f){const s=document.createDocumentFragment(),c=Math.ceil(e.length/a);for(let i=1;i<=c;i++){const t=document.createElement("button");t.classList.add("fav-pagination-btn"),t.textContent=i,t.addEventListener("click",()=>{const l=(i-1)*a,m=l+a,b=e.slice(l,m).map(n=>`<li class="fav-ex-item">
               <div class="fav-ex-item-header">
                   <div class="fav-workout-box">
                       <p class="fav-workout-text">workout</p>
@@ -73,5 +73,5 @@ import{o as g}from"./assets/qoute-df1053f0.js";import"./assets/vendor-8cce9181.j
                       <span class="fav-ex-desc-value">${r(n.target)}</span>
                   </li>
               </ul>
-          </div>`).join("");d.innerHTML=m,g()}),c>9&&o<768?t.classList.add("fav-pagi-btn-overflow"):t.classList.remove("fav-pagi-btn-overflow"),s.appendChild(t)}f.innerHTML="",f.appendChild(s)}const x=document.querySelectorAll(".fav-delete-btn");Array.from(x).forEach(s=>{s.addEventListener("click",()=>{const c=s.closest(".fav-ex-item"),i=c.querySelector(".fav-start-ex-btn").dataset.id;c.remove();const t=e.filter(l=>l._id!==i);localStorage.setItem("favorites",JSON.stringify(t)),e=t})})}function r(e){if(!e.length)return"";const v=e.charAt(0).toUpperCase(),o=e.slice(1);return v+o}b();
+          </div>`).join("");d.innerHTML=b,g()}),c>9&&o<768?t.classList.add("fav-pagi-btn-overflow"):t.classList.remove("fav-pagi-btn-overflow"),s.appendChild(t)}f.innerHTML="",f.appendChild(s)}const h=document.querySelectorAll(".fav-delete-btn");Array.from(h).forEach(s=>{s.addEventListener("click",()=>{const c=s.closest(".fav-ex-item"),i=c.querySelector(".fav-start-ex-btn").dataset.id;c.remove();const t=e.filter(l=>l._id!==i);localStorage.setItem("favorites",JSON.stringify(t)),e=t,x()})})}function r(e){if(!e.length)return"";const v=e.charAt(0).toUpperCase(),o=e.slice(1);return v+o}x();
 //# sourceMappingURL=commonHelpers.js.map
