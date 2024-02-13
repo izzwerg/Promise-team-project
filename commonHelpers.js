6c7796e9646a24f6f88c1c63199cacc44e5e90e4
izzwerg <45130552+izzwerg@users.noreply.github.com>
2024-02-13 02:08:00 +0000

    Deploying to gh-pages from @ izzwerg/Promise-team-project@596c4ee8a74f384f1467c1ef547457fdf8c839db 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,4 +1,4 @@
-import{o as h}from"./assets/qoute-e96c8fe3.js";import"./assets/vendor-8cce9181.js";let t=JSON.parse(localStorage.getItem("favorites"))||[];function v(){const n=t.map(e=>`<li class="fav-ex-item">
+import{o as h}from"./assets/qoute-e96c8fe3.js";import"./assets/vendor-8cce9181.js";function v(){let e=JSON.parse(localStorage.getItem("favorites"))||[];const f=e.map(t=>`<li class="fav-ex-item">
       <div class="fav-ex-item-header">
           <div class="fav-workout-box">
               <p class="fav-workout-text">workout</p>
@@ -8,7 +8,7 @@ import{o as h}from"./assets/qoute-e96c8fe3.js";import"./assets/vendor-8cce9181.j
                     <use href="./assets/sprite-a52c12ca.svg#trash"></use>
                 </svg>
           </button>
-          <button class="fav-start-ex-btn filtered-start-ex-btn" type="button" data-id="${e._id}">Start
+          <button class="fav-start-ex-btn filtered-start-ex-btn" type="button" data-id="${t._id}">Start
               <svg class="fav-start-arrow-icon" width="14" height="14">
                   <use href="./assets/sprite-a52c12ca.svg#arrow"></use>
               </svg>
@@ -20,25 +20,25 @@ import{o as h}from"./assets/qoute-e96c8fe3.js";import"./assets/vendor-8cce9181.j
                   <use href="./assets/sprite-a52c12ca.svg#runner"></use>
               </svg>
           </div>
-          <p class="fav-ex-name">${c(e.name)}</p>
+          <p class="fav-ex-name">${r(t.name)}</p>
       </div>
       <ul class="fav-ex-desc-list">
           <li class="fav-ex-desc-item">Burned calories:
-              <span class="fav-ex-desc-value">${e.burnedCalories} / ${e.time} min</span>
+              <span class="fav-ex-desc-value">${t.burnedCalories} / ${t.time} min</span>
           </li>
           <li class="fav-ex-desc-item">Body part:
-              <span class="fav-ex-desc-value">${c(e.bodyPart)}</span>
+              <span class="fav-ex-desc-value">${r(t.bodyPart)}</span>
           </li>
           <li class="fav-ex-desc-item">Target:
-              <span class="fav-ex-desc-value">${c(e.target)}</span>
+              <span class="fav-ex-desc-value">${r(t.target)}</span>
           </li>
       </ul>
-  </div>`).join(""),f=`
+  </div>`).join(""),o=`
       <div class="no-fav-ex-inner">
         <div class="dumbbell-img"></div>
         <p class="no-fav-text">It appears that you haven't added any exercises to your favorites yet. To get started, you can add exercises that you like to your favorites for easier access in the future.</p>
       </div>
-    `,r=window.innerWidth;let i;r<768?i=8:i=t.length;const g=document.querySelector(".no-fav-ex-inner"),u=document.querySelector(".fav-ex-list"),p=document.querySelector(".fav-pag-btn-set");if(t.length===0?g&&(g.innerHTML=f):u&&(u.innerHTML=n,h()),i<t.length&&p){const e=document.createDocumentFragment(),l=Math.ceil(t.length/i);for(let o=1;o<=l;o++){const s=document.createElement("button");s.classList.add("fav-pagination-btn"),s.textContent=o,s.addEventListener("click",()=>{const d=(o-1)*i,y=d+i,w=t.slice(d,y).map(a=>`<li class="fav-ex-item">
+    `,l=window.innerWidth;let i;l<768?i=8:i=e.length;const m=document.querySelector(".no-fav-ex-inner"),u=document.querySelector(".fav-ex-list"),p=document.querySelector(".fav-pag-btn-set");if(e.length===0?m&&(m.innerHTML=o):u&&(u.innerHTML=f,h()),i<e.length&&p){const t=document.createDocumentFragment(),c=Math.ceil(e.length/i);for(let n=1;n<=c;n++){const s=document.createElement("button");s.classList.add("fav-pagination-btn"),s.textContent=n,s.addEventListener("click",()=>{const d=(n-1)*i,y=d+i,w=e.slice(d,y).map(a=>`<li class="fav-ex-item">
               <div class="fav-ex-item-header">
                   <div class="fav-workout-box">
                       <p class="fav-workout-text">workout</p>
@@ -60,18 +60,18 @@ import{o as h}from"./assets/qoute-e96c8fe3.js";import"./assets/vendor-8cce9181.j
                           <use href="./assets/sprite-a52c12ca.svg#runner"></use>
                       </svg>
                   </div>
-                  <p class="fav-ex-name">${c(a.name)}</p>
+                  <p class="fav-ex-name">${r(a.name)}</p>
               </div>
               <ul class="fav-ex-desc-list">
                   <li class="fav-ex-desc-item">Burned calories:
                       <span class="fav-ex-desc-value">${a.burnedCalories} / ${a.time} min</span>
                   </li>
                   <li class="fav-ex-desc-item">Body part:
-                      <span class="fav-ex-desc-value">${c(a.bodyPart)}</span>
+                      <span class="fav-ex-desc-value">${r(a.bodyPart)}</span>
                   </li>
                   <li class="fav-ex-desc-item">Target:
-                      <span class="fav-ex-desc-value">${c(a.target)}</span>
+                      <span class="fav-ex-desc-value">${r(a.target)}</span>
                   </li>
               </ul>
-          </div>`).join("");u.innerHTML=w,h();const S=document.querySelectorAll(".fav-delete-btn");Array.from(S).forEach(a=>{a.addEventListener("click",()=>{const m=a.closest(".fav-ex-item"),L=m.querySelector(".fav-start-ex-btn").dataset.id;m.remove();let x=t.filter(I=>I._id!==L);localStorage.setItem("favorites",JSON.stringify(x)),t=x,v()})})}),l>9&&r<768?s.classList.add("fav-pagi-btn-overflow"):s.classList.remove("fav-pagi-btn-overflow"),e.appendChild(s)}p.innerHTML="",p.appendChild(e)}const b=document.querySelectorAll(".fav-delete-btn");Array.from(b).forEach(e=>{e.addEventListener("click",()=>{const l=e.closest(".fav-ex-item"),o=l.querySelector(".fav-start-ex-btn").dataset.id;l.remove();let s=t.filter(d=>d._id!==o);localStorage.setItem("favorites",JSON.stringify(s)),t=s,v()})})}function c(n){if(!n.length)return"";const r=n.charAt(0).toUpperCase(),i=n.slice(1);return r+i}v();let E=localStorage.setItem;localStorage.setItem=function(n,f){const r=new Event("itemRemoved");document.dispatchEvent(r),E.apply(this,arguments)};function $(){setTimeout(()=>{t=JSON.parse(localStorage.getItem("favorites")),v()},200)}document.addEventListener("itemRemoved",$);
+          </div>`).join("");u.innerHTML=w,h();const S=document.querySelectorAll(".fav-delete-btn");Array.from(S).forEach(a=>{a.addEventListener("click",()=>{const g=a.closest(".fav-ex-item"),L=g.querySelector(".fav-start-ex-btn").dataset.id;g.remove();let x=e.filter(E=>E._id!==L);localStorage.setItem("favorites",JSON.stringify(x)),e=x,v()})})}),c>9&&l<768?s.classList.add("fav-pagi-btn-overflow"):s.classList.remove("fav-pagi-btn-overflow"),t.appendChild(s)}p.innerHTML="",p.appendChild(t)}const b=document.querySelectorAll(".fav-delete-btn");Array.from(b).forEach(t=>{t.addEventListener("click",()=>{const c=t.closest(".fav-ex-item"),n=c.querySelector(".fav-start-ex-btn").dataset.id;c.remove();let s=e.filter(d=>d._id!==n);localStorage.setItem("favorites",JSON.stringify(s)),e=s,v()})})}function r(e){if(!e.length)return"";const o=e.charAt(0).toUpperCase(),l=e.slice(1);return o+l}v();let I=localStorage.setItem;localStorage.setItem=function(e,f){const o=new Event("itemRemoved");document.dispatchEvent(o),I.apply(this,arguments)};function $(){setTimeout(()=>{v()},200)}document.addEventListener("itemRemoved",$);
 //# sourceMappingURL=commonHelpers.js.map
